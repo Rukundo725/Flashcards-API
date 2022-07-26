f3261141bcc1126c0bd2d58f507b82f1debde3ec
Rukundo725 <gerukundo14@gmail.com>
2022-07-26 23:40:24 +0200

    updated port with an env variable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { ApolloServer } from "apollo-server";
 import { schema } from "./schema";
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export const server = new ApolloServer({
     schema,
 });
 
-const port = 3000;
+const port = process.env.PORT || 5000;
+
 server.listen({port}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
