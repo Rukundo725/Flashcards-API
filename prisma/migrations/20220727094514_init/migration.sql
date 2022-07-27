9effe79d16c021a063229fef873c0bf1f95f7f5b
Rukundo725 <gerukundo14@gmail.com>
2022-07-27 13:07:22 +0200

    created simple migration

---
+++ prisma/migrations/20220727094514_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "topic" TEXT NOT NULL,
+    "question" TEXT NOT NULL
+);
